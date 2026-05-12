378776d2a33dc1e074413deef9f4cf9655e01cda
Karan Vani <karanvani2003@gmail.com>
2026-05-12 14:11:10 +0530

    fix global navbar

--- nexsynchub/src/app/layout.tsx
+++ nexsynchub/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/providers/SessionProvider";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +30,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <AuthProvider>
-
-          {/* 🔥 NAVBAR */}
-          <Navbar />
-
           {/* 🔥 MAIN CONTENT */}
           <main className="flex-1">
             {children}
