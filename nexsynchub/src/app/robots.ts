4fca97276daa6c2fd1ac339830aaaa0022900990
Karan Vani <karanvani2003@gmail.com>
2026-06-21 12:40:27 +0530

    update robot.ts base url

--- nexsynchub/src/app/robots.ts
+++ nexsynchub/src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://nexsynchub.com";
+  const baseUrl = "https://www.nexsynchub.com";
 
   return {
     rules: [
